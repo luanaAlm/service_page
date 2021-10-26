70b38128fc8ed2974d1b217d3d329aa10a513fb3
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-10-26 11:10:17 -0300

    Home e navbar

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    /* ---Navbar--- */
+    $(window).on("scroll", function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink")
+        }
+    });
+})
