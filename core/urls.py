7d0fe7ff98199e9a237cbe8daeb35bda0b601ec6
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-11-12 15:11:33 -0300

    Criação de páginas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import index, cliente_novo, email
-
+from .views import Duvidas, index, cliente_novo, landingPage, lojaVirtual, siteInstitucional, socialMedia, sobreNos, Portifolio, Contato, Orcamento
 
 urlpatterns = [
     path('', index, name='index'),
     path('cliente_novo/', cliente_novo, name='cliente_novo'),
-    path('email/', email, name='email'),
+    # serviços
+    path('landing_page', landingPage, name='landing_page'),
+    path('site_institucional', siteInstitucional, name='site_institucional'),
+    path('loja_virtual', lojaVirtual, name='loja_virtual'),
+    path('social_media', socialMedia, name='social_media'),
+    # Sobre nós
+    path('sobre_nos', sobreNos, name='sobre_nos'),
+    # Portifolio
+    path('portifolio', Portifolio, name='portifolio'),
+    # duvidas
+    path('duvidas', Duvidas, name='duvidas'),
+    # contato
+    path('contato', Contato, name='contato'),
+    # orcamento
+    path('orcamento', Orcamento, name='orcamento'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,11 +6,10 @@ from django.shortcuts import redirect, render
 
 
 def index(request):
-    portifolios = Portifolio.objects.all()
+    #portifolios = Portifolio.objects.all()
     form = ClienteForm()
     return render(request, "index.html", {
-        "form": form, "portifolios": portifolios
-    })
+        "form": form})
 
 
 def cliente_novo(request):
@@ -25,5 +24,37 @@ def cliente_novo(request):
         return redirect('index')
 
 
-def email(request):
-    return render(request, "email.html")
+def landingPage(request):
+    return render(request, "servicos/landing_page.html")
+
+
+def siteInstitucional(request):
+    return render(request, "servicos/site_institucional.html")
+
+
+def lojaVirtual(request):
+    return render(request, "servicos/loja_virtual.html")
+
+
+def socialMedia(request):
+    return render(request, "servicos/social_media.html")
+
+
+def sobreNos(request):
+    return render(request, "sobre_nos.html")
+
+
+def Portifolio(request):
+    return render(request, "portifolio.html")
+
+
+def Duvidas(request):
+    return render(request, "duvidas.html")
+
+
+def Contato(request):
+    return render(request, "contato.html")
+
+
+def Orcamento(request):
+    return render(request, "orcamento.html")
