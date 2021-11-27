e574633310f474db7f3882e3e4069eccbeea8d9b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-11-27 16:25:50 -0300

    Alterações

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Cliente,  Portifolio_cliente, Social_Media, Depoimento
+from .models import Cliente, Depoimento, Portifolio_cliente, Social_Media
 
 admin.site.register(Cliente)
+admin.site.register(Depoimento)
 admin.site.register(Portifolio_cliente)
 admin.site.register(Social_Media)
-admin.site.register(Depoimento)
