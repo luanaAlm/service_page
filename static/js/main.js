6741dc7e4b6b4c6037d30cc937aec8415e9278a6
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-02-14 13:50:39 -0300

    Cookies

--- static/js/main.js
+++ static/js/main.js
@@ -56,4 +56,19 @@ $(document).ready(function(){
         $(".navbar-collapse").collapse("hide");
     });
     
+    /* --- Cookies --- */
+    const cookieContainer = document.querySelector(".cookie-container")
+    const cookieButton = document.querySelector(".cookie-btn")  
+
+    cookieButton.addEventListener("click", () =>{
+        cookieContainer.classList.remove("active");
+        localStorage.setItem("cookieBannerDisplayed", "true")
+    });
+    setTimeout(() => {
+        if(!localStorage.getItem("cookieBannerDisplayed")){
+            cookieContainer.classList.add("active");
+        }
+    }, 2000);
+
+
 });
